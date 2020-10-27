fe8cf443277e911f33e2bc72ce8995a69d726a5e
Pavel Shakhlovich <altmersc@gmail.com>
2020-10-27 10:03:16 +0300

    first commit

---
+++ src/main/java/com/codewars/altmer/StringSplit.java
@@ -0,0 +1,8 @@
+package com.codewars.altmer;
+
+public class StringSplit {
+  public static String[] solution(String s) {
+    //Write your code here
+    return new String[0];
+  }
+}
---
+++ src/test/java/com/codewars/altmer/StringSplitTest.java
@@ -0,0 +1,28 @@
+package com.codewars.altmer;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class StringSplitTest {
+
+  @Test
+  public void testEvenString() {
+    String s = "abcdef";
+    String s1 = "HelloWorld";
+    assertEquals("Should handle even string","[ab, cd, ef]", Arrays.toString(StringSplit.solution(s)));
+    assertEquals("Should handle even string","[He, ll, oW, or, ld]", Arrays.toString(StringSplit.solution(s1)));
+  }
+
+  @Test
+  public void testOddString() {
+    String s = "abcde";
+    String s1 = "LovePizza";
+    assertEquals("Should handle odd string","[ab, cd, e_]", Arrays.toString(StringSplit.solution(s)));
+    assertEquals("Should handle odd string","[Lo, ve, Pi, zz, a_]", Arrays.toString(StringSplit.solution(s1)));
+  }
+}
